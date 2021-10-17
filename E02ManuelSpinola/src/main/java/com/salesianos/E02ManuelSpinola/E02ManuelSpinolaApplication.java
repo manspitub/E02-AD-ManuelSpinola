fb3e1ea41c2d5a35f023bf4dfc940520f2e9daf3
Manuel Spinola Tubio <spinola.tuman21@triana.salesianos.edu>
2021-10-18 00:27:14 +0200

    fallo solucionado y habilitada consola H2

---
+++ E02ManuelSpinola/src/main/java/com/salesianos/E02ManuelSpinola/E02ManuelSpinolaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.E02ManuelSpinola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E02ManuelSpinolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E02ManuelSpinolaApplication.class, args);
+	}
+
+}
---
+++ E02ManuelSpinola/src/test/java/com/salesianos/E02ManuelSpinola/E02ManuelSpinolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.E02ManuelSpinola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E02ManuelSpinolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
